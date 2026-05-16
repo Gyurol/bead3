260ddf90e290d089c8ac3a3dedf58e2f0f519f05
Gyurol <macskajani2@gmail.hu>
2026-05-16 08:32:27 +0200

    Revert "felesleges fajlok torlese, extra simitasok"
    This reverts commit 02ee3981c6ad1b0b2bf27410fc87dfafd842a324.

---
+++ Start.hpp
@@ -0,0 +1,17 @@
+#ifndef START_HPP
+#define START_HPP
+
+
+
+class Start{
+
+public:
+    Start(Application *, int,int,int,int,std::string);
+    void draw() override;
+    void handle() override;
+
+};
+
+
+
+#endif // START_HPP
